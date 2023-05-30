ff7e292fb490d363904f82f7f51d8a54ca46ef96
PedroPenha <pedro.eng98@gmail.com>
2023-05-30 00:47:57 -0300

    js_modal_config

--- script.js
+++ script.js
@@ -49,8 +49,17 @@ function getDragAfterElement(container, y) {
 
 function abrirModal(carregarModal){
   console.log("Carregar a janela modal: " + carregarModal);
+  let modal = document.getElementById(carregarModal);
+
+  modal.style.display = 'block';
+  document.body.style.overflow = 'hidden';
+  document.getElementById
 }
 
 function fecharModal(fecharModal){
   console.log("Fechar a janela modal: " + fecharModal);
+  let modal = document.getElementById(fecharModal);
+
+  modal.style.display = 'none';
+  document.body.style.overflow = 'auto';
 }
